275452016a53c41ddf15ea18b5cc55fd7837d5dd
lzhupelmorex <lzhu@pelmorex.com>
2019-02-11 13:23:20 -0500

    launch detail activity at item clicked

--- app/src/main/java/com/example/reddit/RedditAdapter.kt
+++ app/src/main/java/com/example/reddit/RedditAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.reddit
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -14,7 +16,15 @@ class RedditAdapter (private var redditList: ArrayList<RedditItem>) : RecyclerVi
     lateinit var context : Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RedditItemViewHolder {
         this.context = parent.context
-        return RedditItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.grid_item, parent, false))
+        val holder = RedditItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.grid_item, parent, false))
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, RedditDetailedActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
+            context.startActivity(intent)
+
+        }
+        return holder
     }
 
     override fun getItemCount(): Int {
@@ -22,8 +32,8 @@ class RedditAdapter (private var redditList: ArrayList<RedditItem>) : RecyclerVi
     }
 
     override fun onBindViewHolder(holder: RedditItemViewHolder, position: Int) {
-        holder?.titleTextView.text =  redditList[position].title
-        holder?.authorTextView.text = redditList[position].author
+        holder?.titleTextView.text =  "Title: " + redditList[position].title
+        holder?.authorTextView.text = "Author: " + redditList[position].author
         if (!redditList[position].thumbnail.contains("http")) {
             holder?.image.setImageDrawable(context.getDrawable(R.drawable.ic_launcher_background))
         } else {
